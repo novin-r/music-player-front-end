a9807f26bc4da64afa8a1b1b455732d39cb5d9a8
novin ramzi <a20novra@student.his.se>
2023-01-29 16:54:38 +0100

    Rearrange playlists based on the last played item

--- src/Components/PlayListSec.jsx
+++ src/Components/PlayListSec.jsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react'
 import styles from "./PlaylistSec.module.css";
 
-function PlayListSec({playlist,playlistId ,setPlaylistid, setselectePlaylist}) {
+function PlayListSec({playlist,playlistId ,setPlaylistid, setselectePlaylist, onPlay}) {
 
     const handleClick = () => {
         setPlaylistid(playlistId);
         setselectePlaylist(playlist);
+        onPlay();
     };
 
   return (
